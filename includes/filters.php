bc109993544dab3756e593ce9906aa60a89108a8
allycol <allycolquhoun@gmail.com>
2015-07-15 11:33:22 +1000

    Adding off-canvas for filters

--- includes/filters.php
+++ includes/filters.php
@@ -15,43 +15,63 @@
         <li><span><input type=checkbox name="brand19" id="brand19"><label for="brand19">Snugpak</label></span></li>
         <li><span><input type=checkbox name="brand2" id="brand2"><label for="brand2">The North Face</label></span></li>
         <li><span><input type=checkbox name="brand90" id="brand90"><label for="brand90">Under Armour</label></span></li>
+        <li>
+          <span>
+            <a href="#mm-10" class="button">Done</a>
+          </span>
+        </li>
       </ul>
     </li>
     <li>
       <span>Product type:</span>
       <ul>
         <li><span><input type=checkbox name="master348" onClick="this.form.submit()"><label for="">3-in-1's & Triclimates</label></span></li>
-        <li><input type=checkbox name="master713" onClick="this.form.submit()"><label for="">Baselayer & Thermal Tops</label></li>
-        <li><input type=checkbox name="master212" onClick="this.form.submit()"><label for="">Bodywarmers</label></li>
-        <li><input type=checkbox name="master210" onClick="this.form.submit()"><label for="">Down Jackets</label></li>
-        <li><input type=checkbox name="master2332" onClick="this.form.submit()"><label for="">Gloves & Mitts</label></li>
-        <li><input type=checkbox name="master2006,2327" onClick="this.form.submit()"><label for="">Insulated</label></li>
-        <li><input type=checkbox name="master221" onClick="this.form.submit()"><label for="">Jackets (200 weight)</label></li>
-        <li><input type=checkbox name="master213" onClick="this.form.submit()"><label for="">Legwear</label></li>
-        <li><input type=checkbox name="master211" onClick="this.form.submit()"><label for="">Parkas</label></li>
-        <li><input type=checkbox name="master2655" onClick="this.form.submit()"><label for="">Ponchos</label></li>
-        <li><input type=checkbox name="master981" onClick="this.form.submit()"><label for="">Synthetic Jackets</label></li>
-        <li><input type=checkbox name="master114" onClick="this.form.submit()"><label for="">Technical Tops - Short Sleeve</label></li>
-        <li><input type=checkbox name="master2326" onClick="this.form.submit()"><label for="">Town & Casual</label></li>
-        <li><input type=checkbox name="master86" onClick="this.form.submit()"><label for="">Trousers</label></li>
-        <li><input type=checkbox name="master3027,3028" onClick="this.form.submit()"><label for="">Waterproof</label></li>
+        <li><span><input type=checkbox name="master713" onClick="this.form.submit()"><label for="">Baselayer & Thermal Tops</label></span></li>
+        <li><span><input type=checkbox name="master212" onClick="this.form.submit()"><label for="">Bodywarmers</label></span></li>
+        <li><span><input type=checkbox name="master210" onClick="this.form.submit()"><label for="">Down Jackets</label></span></li>
+        <li><span><input type=checkbox name="master2332" onClick="this.form.submit()"><label for="">Gloves & Mitts</label></span></li>
+        <li><span><input type=checkbox name="master2006,2327" onClick="this.form.submit()"><label for="">Insulated</label></span></li>
+        <li><span><input type=checkbox name="master221" onClick="this.form.submit()"><label for="">Jackets (200 weight)</label></span></li>
+        <li><span><input type=checkbox name="master213" onClick="this.form.submit()"><label for="">Legwear</label></span></li>
+        <li><span><input type=checkbox name="master211" onClick="this.form.submit()"><label for="">Parkas</label></span></li>
+        <li><span><input type=checkbox name="master2655" onClick="this.form.submit()"><label for="">Ponchos</label></span></li>
+        <li><span><input type=checkbox name="master981" onClick="this.form.submit()"><label for="">Synthetic Jackets</label></span></li>
+        <li><span><input type=checkbox name="master114" onClick="this.form.submit()"><label for="">Technical Tops - Short Sleeve</label></span></li>
+        <li><span><input type=checkbox name="master2326" onClick="this.form.submit()"><label for="">Town & Casual</label></span></li>
+        <li><span><input type=checkbox name="master86" onClick="this.form.submit()"><label for="">Trousers</label></span></li>
+        <li><span><input type=checkbox name="master3027,3028" onClick="this.form.submit()"><label for="">Waterproof</label></span></li>
+        <li>
+          <span>
+            <a href="#mm-10" class="button">Done</a>
+          </span>
+        </li>
       </ul>
     </li>
     <li>
       <span>Gender:</span>
       <ul>
-        <li><input type=checkbox name="gendermen" onClick="this.form.submit()"><label for="">men</label></li>
-        <li><input type=checkbox name="genderwomen" onClick="this.form.submit()"><label for="">women</label></li>
+        <li><span><input type=checkbox name="gendermen" onClick="this.form.submit()"><label for="">men</label></li>
+        <li><span><input type=checkbox name="genderwomen" onClick="this.form.submit()"><label for="">women</label></li>
+        <li>
+          <span>
+            <a href="#mm-10" class="button">Done</a>
+          </span>
+        </li>
       </ul>
     </li>
     <li>
       <span>Price:</span>
       <ul>
-        <li><input type=checkbox name="price1" onClick="this.form.submit()"><label for="">&pound;1 - &pound;24.99</label></li>
-        <li><input type=checkbox name="price2" onClick="this.form.submit()"><label for="">&pound;25 - &pound;49.99</label></li>
-        <li><input type=checkbox name="price3" onClick="this.form.submit()"><label for="">&pound;50 - &pound;99.99</label></li>
-        <li><input type=checkbox name="price4" onClick="this.form.submit()"><label for="">&pound;100 - &pound;149.99</label></li>
-        <li><input type=checkbox name="price5" onClick="this.form.submit()"><label for="">&pound;150 - &pound;199.99</label></li>
+        <li><span><input type=checkbox name="price1" onClick="this.form.submit()"><label for="">&pound;1 - &pound;24.99</label></li>
+        <li><span><input type=checkbox name="price2" onClick="this.form.submit()"><label for="">&pound;25 - &pound;49.99</label></li>
+        <li><span><input type=checkbox name="price3" onClick="this.form.submit()"><label for="">&pound;50 - &pound;99.99</label></li>
+        <li><span><input type=checkbox name="price4" onClick="this.form.submit()"><label for="">&pound;100 - &pound;149.99</label></li>
+        <li><span><input type=checkbox name="price5" onClick="this.form.submit()"><label for="">&pound;150 - &pound;199.99</label></li>
+        <li>
+          <span>
+            <a href="#mm-10" class="button">Done</a>
+          </span>
+        </li>
       </ul>
     </li>
     <li>
